2fd8c89d62d1087945f12b8665dc84a72354538e
iMac G1 <imacg1@iMac-de-Energy-28.local>
2022-05-09 10:50:07 +0000

    code commenté

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 function bouton() {
+    // récupération et affichage du nom 
     let nom = document.querySelector("#nom");
     let recuperer = document.querySelector("#recuperer");
 
     recuperer.innerHTML = nom.value;
 
+    // récupération et affichage du mail
     let mail = document.querySelector("#mail");
     let recueil = document.querySelector("#recueil");
 
     recueil.innerHTML = mail.value;
 
-    let number = document.querySelector("#number");
-    let numerotaion = document.querySelector("#numerotaion");
+    // recupération et affichage de la satisfaction par numérotation
+    let number = document.querySelector("#nombre");
+    let numerotation = document.querySelector("#numerotation");
 
-    numerotaion.innerHTML = number.value;
+    numerotation.innerHTML = number.value;
 
+    // récupération et affichage du nom du coach
     let name = document.querySelector("#name");
     let stock = document.querySelector("#stock");
 
     stock.innerHTML = name.value;
 
+// récupératin et affichage de l'expérience
     let type = document.querySelector("#type");
     let description = document.querySelector("#description");
 
